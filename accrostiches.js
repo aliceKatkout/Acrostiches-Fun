a74bff9d519fe0161392215688785cbce7735c51
Alice <alice.katkout@gmail.com>
2020-10-14 11:59:42 +0200

    Update accrostiches.js
    Maintenant c'est un adjectif aléatoire

--- accrostiches.js
+++ accrostiches.js
@@ -1,37 +1,40 @@
 const adjectifs = {
-	A: "Adorable",
-	B: "Barbare",
-	C: "Créati.f.ve",
-	D: "Drôle",
-	E: "Extraordinaire",
-	F: "Fabuleu.x.se",
-	G: "Génial.e",
-	H: "Horrible",
-	I: "Irrépressible",
-	J: "Joyeu.x.se",
-	K: "Kamikaze",
-	L: "Lardon",
-	M: "Marrant.e",
-	N: "Nyctalope",
-	O: "Optimiste",
-	P: "Provoquant.e",
-	Q: "Quadrilatère",
-	R: "Rêveu.r.se",
-	S: "Sympathique",
-	T: "Talentueu.x.se",
-	U: "Unique",
-	V: "Véritable",
-	W: "Wagon",
-	X: "X-trême",
-	Y: "Youhou",
-	Z: "Zoyeu.x.se"
+	A: ["Adorable", "Amical.e", "Agil.e", "Arrogant.e", "Audacieux.se","Attentive", "Authentique"],
+	B: ["Barbare", "Belle", "Brave", "Bocal"],
+	C: ["Cupide", "Classe", "Comique", "Cocasse", "Créatif.ve", "Calme", "Carré.e"],
+	D: ["Drôle", "Dormeur.se", "Dictateur.trice", "Délirant.e", "Docile"],
+	E: ["Exceptionnel.le", "Elégant.e", "Erudit.e", "Extraordinaire", "Eclatant.e", "Enthousiaste"],
+	F: ["Fort.e", "Fantastique", "Frimeur.se", "Fabuleux.se", "Formidable"],
+	G: ["Génial.e", "Généreux.se", "Gentil.le", "Grand.e", "Glauque"],
+	H: ["Horrible", "Hackeur", "Hospitalier"],
+	I: ["Irresistible", "Idiot.e", "Idyllique","Invincible", "Irrépressible"],
+	J: ["Joyeu.se", "Joli.e", "Joueur.euse"],
+	K: ["Kanon", "Kamikaze", "Karatéka"],
+	L: ["Loyal.e", "Logique", "Looping", "Lardon"],
+	M: ["Magnifique", "Mignon.ne", "Myope", "Marrant.e", "Merveilleux.se"],
+	N: ["Naif.ve", "Novateur.trice", "Narquois.e","Negatif.ve","Noble","Narratif.ve","Nice", "Net.te", "Nyctalope"],
+	O: ["Optimiste", "Original.e", "Organisé.e", "Orage"],
+	P: ["Parfait.e", "Propre", "Purifié.e", "Provoquant.e", "Périlleux.se"],
+	Q: ["Qualitatif.ve", "Qomique", "Quadrilatère"],
+	R: ["Responsable", "Raisonnable", "Rieur.euse", "Rêveur.se", "Royal.e"],
+	S: ["Super", "Sympathique", "Salvateur.trice", "Souriant.e", "Stupide"],
+	T: ["Triste", "Terrifiant.e", "Torturé.e", "Taquin.e", "Trivial.e","Touche-à-tout", "Tendre", "Talentueux.se", "Taquin.e", "Télépathe"],
+	U: ["Unique", "Utopiste", "Usurpateur.trice"],
+	V: ["Vrai.e", "Vénéré.e", "Vaniteux.se", "Vénéneux.se", "Véritable"],
+	W: ["Wow", "Waouw", "Wapiti", "Wolksvagen" ],
+	X: ["Xylophone", "Xtra'comme les kellogs","Xtrème"],
+	Y: ["Youpi", "Youhou", "Yougoslave","Yikes"],
+	Z: ["Zinzin", "Zébré.e", "Zelé.e", "Zoyeux.se"]
 }
 
 let prenom = "Alice" ;
 let prenom_maj = prenom.toUpperCase();
 let liste_lettres = Array.from(prenom_maj);
 
+
+
 for (let lettre of liste_lettres) {
 	let acro = adjectifs[lettre];
-	console.log(acro);
+	var acro_random = acro[(Math.random()*acro.length)|0];
+	console.log(acro_random);
 }
